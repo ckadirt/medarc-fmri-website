457c436078b6b569ad9abb2f9916eaf151249488
Cesar Kadir Torrico Villanueva <torrico.villanueva.cesar.kadir@gmail.com>
2023-11-28 18:42:45 -0400

    Basic animation

--- src/script.js
+++ src/script.js
@@ -5,6 +5,8 @@ import * as THREE from 'three'
 import { TWEEN } from 'three/examples/jsm/libs/tween.module.min.js'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+
+import { MeshSurfaceSampler } from 'three/examples/jsm/math/MeshSurfaceSampler.js'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
 
 /////////////////////////////////////////////////////////////////////////
@@ -17,14 +19,13 @@ loader.setDRACOLoader(dracoLoader)
 
 /////////////////////////////////////////////////////////////////////////
 ///// DIV CONTAINER CREATION TO HOLD THREEJS EXPERIENCE
-const container = document.createElement('div')
+const container = document.getElementById( 'canvas' );
 document.body.appendChild(container)
 
 /////////////////////////////////////////////////////////////////////////
 ///// SCENE CREATION
 const scene = new THREE.Scene()
-scene.background = new THREE.Color('#c8f0f9')
-
+scene.background = new THREE.Color('#000000')
 /////////////////////////////////////////////////////////////////////////
 ///// RENDERER CONFIG
 const renderer = new THREE.WebGLRenderer({ antialias: true}) // turn on antialias
@@ -35,8 +36,9 @@ container.appendChild(renderer.domElement) // add the renderer to html div
 
 /////////////////////////////////////////////////////////////////////////
 ///// CAMERAS CONFIG
-const camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1, 100)
-camera.position.set(34,16,-20)
+const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 100)
+camera.position.set(34,16,20)
+camera.lookAt(new THREE.Vector3(1000,1000,1000));
 scene.add(camera)
 
 /////////////////////////////////////////////////////////////////////////
@@ -66,27 +68,101 @@ scene.add(sunLight)
 
 /////////////////////////////////////////////////////////////////////////
 ///// LOADING GLB/GLTF MODEL FROM BLENDER
-loader.load('models/gltf/starter-scene.glb', function (gltf) {
+loader.load('models/gltf/bff.glb', function (gltf) {
 
-    scene.add(gltf.scene)
+    gltf.scene.traverse((obj) => {
+        if (obj.isMesh) {
+            sampler = new MeshSurfaceSampler(obj).build()
+        }
+    })
+
+    transformMesh()
 })
+
+
+/////////////////////////////////////////////////////////////////////////
+///// TRANSFORM MESH INTO POINTS
+let sampler
+let uniforms = { mousePos: {value: new THREE.Vector3()}}
+let pointsGeometry = new THREE.BufferGeometry()
+const cursor = {x:0, y:0}
+const vertices = []
+const tempPosition = new THREE.Vector3()
+
+function transformMesh(){
+    // Loop to sample a coordinate for each points
+    for (let i = 0; i < 90000; i ++) {
+        // Sample a random position in the model
+        sampler.sample(tempPosition)
+        // Push the coordinates of the sampled coordinates into the array
+        vertices.push(tempPosition.x, tempPosition.y, tempPosition.z)
+    }
+    
+    // Define all points positions from the previously created array
+    pointsGeometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3))
+
+    // Define the matrial of the points
+    const pointsMaterial = new THREE.PointsMaterial({
+        color:'#ffffff',
+        size: 0.03,
+        blending: THREE.AdditiveBlending,
+        transparent: true,
+        opacity: 0.8,
+        depthWrite: false,
+        sizeAttenuation: true,
+        alphaMap: new THREE.TextureLoader().load('particle-texture.jpg')
+    })
+
+    // Create the custom vertex shader injection
+    pointsMaterial.onBeforeCompile = function(shader) {
+        shader.uniforms.mousePos = uniforms.mousePos
+        
+        shader.vertexShader = `
+          uniform vec3 mousePos;
+          varying float vNormal;
+          
+          ${shader.vertexShader}`.replace(
+          `#include <begin_vertex>`,
+          `#include <begin_vertex>   
+            vec3 seg = position - mousePos;
+            vec3 dir = normalize(seg);
+            float dist = length(seg);
+            if (dist < 100.5){
+              float force = clamp(1.0 / (dist * dist), -0.8, .8);
+              transformed += dir * force;
+              vNormal = force /0.9;
+            }
+          `
+        )
+    }
+
+    // Create an instance of points based on the geometry & material
+    const points = new THREE.Points(pointsGeometry, pointsMaterial)
+
+    // Add them into the main group
+    scene.add(points)
+
+}
+
 
 /////////////////////////////////////////////////////////////////////////
 //// INTRO CAMERA ANIMATION USING TWEEN
 function introAnimation() {
     controls.enabled = false //disable orbit controls to animate the camera
     
-    new TWEEN.Tween(camera.position.set(26,4,-35 )).to({ // from camera position
-        x: 16, //desired x position to go
-        y: 50, //desired y position to go
-        z: -0.1 //desired z position to go
-    }, 6500) // time take to animate
+    new TWEEN.Tween(camera.position.set(10, -30, 10)).to({ // from camera position
+        x: 0.4, //desired x position to go
+        y: -0.8, //desired y position to go
+        z: 4 //desired z position to go
+    }, 500) // time take to animate
     .delay(1000).easing(TWEEN.Easing.Quartic.InOut).start() // define delay, easing
     .onComplete(function () { //on finish animation
         controls.enabled = true //enable orbit controls
         setOrbitControlsLimits() //enable controls limits
+        //controls.camera.rotate.set(0,0,0) //reset camera rotation
         TWEEN.remove(this) // remove the animation from memory
     })
+
 }
 
 introAnimation() // call intro animation on start
@@ -95,26 +171,61 @@ introAnimation() // call intro animation on start
 //// DEFINE ORBIT CONTROLS LIMITS
 function setOrbitControlsLimits(){
     controls.enableDamping = true
-    controls.dampingFactor = 0.04
-    controls.minDistance = 35
-    controls.maxDistance = 60
+    controls.dampingFactor = 0.4
+    controls.minDistance = 0
+    controls.maxDistance = 90
     controls.enableRotate = true
     controls.enableZoom = true
-    controls.maxPolarAngle = Math.PI /2.5
+    controls.zoomSpeed = 0.5
+    controls.autoRotate = true
+}
+
+function onMouseWheel(event){
+
 }
 
 /////////////////////////////////////////////////////////////////////////
 //// RENDER LOOP FUNCTION
-function rendeLoop() {
+let lastMouseMoveTime = Date.now();
+const idleThreshold = 300; // Time in milliseconds after which the mouse is considered idle
+const decayRate = 0.99 // Rate at which the cursor position decays
+const minMovementThreshold = 0.001; // Minimum threshold for cursor movement
+//const cursor = { x: 0, y: 0 };
+const targetCursor = { x: 0, y: 0 }; // Target position for the cursor
+const lerpFactor = 0.1; // Factor for linear interpolation
 
-    TWEEN.update() // update animations
+function renderLoop() {
+    TWEEN.update();
+    controls.update();
+    renderer.render(scene, camera);
 
-    controls.update() // update orbit controls
+    const currentTime = Date.now();
+    if (currentTime - lastMouseMoveTime > idleThreshold) {
+        // Gradually move cursor values towards 0
+        if (Math.abs(cursor.x) > minMovementThreshold || Math.abs(cursor.y) > minMovementThreshold) {
+            cursor.x *= decayRate;
+            cursor.y *= decayRate;
+        }
+    } else {
+        // Interpolate towards the target cursor position when mouse is moving
+        cursor.x += (targetCursor.x - cursor.x) * lerpFactor;
+        cursor.y += (targetCursor.y - cursor.y) * lerpFactor;
+    }
 
-    renderer.render(scene, camera) // render the scene using the camera
+    // Update uniforms with the new cursor position
+    uniforms.mousePos.value.set(cursor.x, cursor.y, 0);
 
-    requestAnimationFrame(rendeLoop) //loop the render function
-    
+    requestAnimationFrame(renderLoop);
 }
 
-rendeLoop() //start rendering
+renderLoop();
+
+document.addEventListener('mousemove', (event) => {
+    event.preventDefault();
+
+    // Set the target cursor position
+    targetCursor.x = (event.clientX / window.innerWidth) - 0.5;
+    targetCursor.y = event.clientY / window.innerHeight - 0.5;
+
+    lastMouseMoveTime = Date.now();
+}, false);
