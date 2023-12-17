0bf0f8a1f536a1ed3a8cecf2a09ed4d1c790791a
Cesar Kadir Torrico Villanueva <torrico.villanueva.cesar.kadir@gmail.com>
2023-12-17 15:05:19 -0400

    effect back

--- src/image-utils.js
+++ src/image-utils.js
@@ -5,7 +5,24 @@ const ImageEffectComponent = ({ imagePath, isMobile }) => {
   const portraitRef = useRef(null);
   const distanceMovementX = isMobile ? "10px" : "15px";
   const distanceMovementY = isMobile ? "7px" : "10px"
-   
+  useEffect(() => {
+    const mouseMonitor = (e) => {
+      let x = e.clientX / window.innerWidth;
+      let y = e.clientY / window.innerHeight;
+      let moveX = x > 0.5 ? '-' + distanceMovementX : distanceMovementX;
+      let moveY = y > 0.5 ? '-' + distanceMovementY : distanceMovementY;
+      portraitRef.current.style.setProperty("--translate-x", moveX);
+      portraitRef.current.style.setProperty("--translate-y", moveY);
+    };
+
+    const portrait = portraitRef.current;
+    portrait.addEventListener("mousemove", mouseMonitor);
+
+    return () => {
+      portrait.removeEventListener("mousemove", mouseMonitor);
+    };
+  }, []);
+
   return (
     <div className="portrait" ref={portraitRef}>
       <div className="fill">
